f434fdb3513db9458a9ecb8cf2c6ec7e489c4065
Henrique Santos Tavares <henriquechr11@gmail.com>
2025-08-20 18:23:07 -0300

    Add files via upload

---
+++ js/bests.js
@@ -0,0 +1,11 @@
+function glow() {
+
+    let corpo = document.body
+    corpo.classList.toggle("dark")
+
+
+}
+
+
+const video = document.getElementById("vf");
+video.playbackRate = 0.5;
---
+++ js/contact.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('a');
+btn.addEventListener('mousemove',e =>{
+    let react =e.clientX *3 - react.left;
+    btn.style.setProperty('--x',x + 'deg');
+}
+
+)
